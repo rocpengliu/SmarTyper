53c2dbc3bbd7f4c40b22d6c995d3f3632641d6cb
rocpengliu <rocpengliu@gmail.com>
2026-09-16 13:53:10 -0500

    minor update

--- src/snpscanner.cpp
+++ src/snpscanner.cpp
@@ -106,41 +106,82 @@ void SnpScanner::groupScanVar(Read* & r1) {
         bool goRP = false;
         int trimF = 0;
         int fpMismatches = (int) edit_distance(locSnpItGrp->fp.mStr.c_str(), locSnpItGrp->fp.length(), r1->mSeq.mStr.c_str(), locSnpItGrp->fp.length());
-        fpData = locSnpItGrp->fp.mStr.c_str();
-        fpLength = locSnpItGrp->fp.length();
-        auto endBoolF = doPrimerAlignment(fpData, fpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
-
-        if(std::min(fpMismatches, get<0>(endBoolF)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq) continue;
-
-        if(get<0>(endBoolF) <= fpMismatches){
-            fpMismatches =  get<0>(endBoolF);
-            if (get<2>(endBoolF) && (get<1>(endBoolF) <= r1->length())) {
-                if ((get<1>(endBoolF) + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= r1->mSeq.length()) {
-                    trimF = get<1>(endBoolF);
+        
+        if(fpMismatches != 0){
+            fpData = locSnpItGrp->fp.mStr.c_str();
+            fpLength = locSnpItGrp->fp.length();
+            auto endBoolF = doPrimerAlignment(fpData, fpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
+            if(std::min(fpMismatches, get<0>(endBoolF)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq){
+                continue;
+            } else {
+                if(fpMismatches < get<0>(endBoolF)){
+                    trimF = locSnpItGrp->fp.length();
                     goRP = true;
+                } else {
+                    fpMismatches = get<0>(endBoolF);
+                    if (get<2>(endBoolF) && (get<1>(endBoolF) <= r1->length())) {
+                        if ((get<1>(endBoolF) + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= r1->mSeq.length()) {
+                            trimF = get<1>(endBoolF);
+                            goRP = true;
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        continue;
+                    }
                 }
             }
         } else {
             trimF = locSnpItGrp->fp.length();
             goRP = true;
         }
+        
+        // fpData = locSnpItGrp->fp.mStr.c_str();
+        // fpLength = locSnpItGrp->fp.length();
+        // auto endBoolF = doPrimerAlignment(fpData, fpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
+
+        // if(std::min(fpMismatches, get<0>(endBoolF)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq) continue;
+
+        // if(get<0>(endBoolF) <= fpMismatches){
+        //     fpMismatches =  get<0>(endBoolF);
+        //     if (get<2>(endBoolF) && (get<1>(endBoolF) <= r1->length())) {
+        //         if ((get<1>(endBoolF) + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= r1->mSeq.length()) {
+        //             trimF = get<1>(endBoolF);
+        //             goRP = true;
+        //         }
+        //     }
+        // } else {
+        //     trimF = locSnpItGrp->fp.length();
+        //     goRP = true;
+        // }
 
         if (goRP) {
             MatchTrim mTrim;
             int rpMismatches = (int) edit_distance(locSnpItGrp->rp.mStr.c_str(), locSnpItGrp->rp.length(), r1->mSeq.mStr.c_str() + r1->mSeq.length() - locSnpItGrp->rp.length(), locSnpItGrp->rp.length());
-            rpData = locSnpItGrp->rp.mStr.c_str();
-            rpLength = locSnpItGrp->rp.length();
-            auto endBoolR = doPrimerAlignment(rpData, rpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
-
-            if(std::min(rpMismatches, get<0>(endBoolR)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq) continue;
-            
-            if(get<0>(endBoolR) <= rpMismatches){
-                if (get<2>(endBoolR) && (get<1>(endBoolR) <= r1->mSeq.mStr.length()) &&
-                        ((trimF + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= get<1>(endBoolR))) {
-                    mTrim.totMismatches = fpMismatches + rpMismatches;
-                    mTrim.trimF = trimF;
-                    mTrim.trimedRefLenth = get<1>(endBoolR) - trimF - locSnpItGrp->rp.mStr.length();
-                    locMap[locSnpItGrp->name] = mTrim;
+            if(rpMismatches != 0) {
+                rpData = locSnpItGrp->rp.mStr.c_str();
+                rpLength = locSnpItGrp->rp.length();
+                auto endBoolR = doPrimerAlignment(rpData, rpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
+                if(std::min(rpMismatches, get<0>(endBoolR)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq){
+                    continue;
+                } else {
+                    if(rpMismatches < get<0>(endBoolR)) {
+                        mTrim.totMismatches = fpMismatches + rpMismatches;
+                        mTrim.trimF = trimF;
+                        mTrim.trimedRefLenth = r1->mSeq.length() - trimF - locSnpItGrp->rp.mStr.length();
+                        locMap[locSnpItGrp->name] = mTrim;
+                    } else {
+                        rpMismatches = get<0>(endBoolR);
+                        if (get<2>(endBoolR) && (get<1>(endBoolR) <= r1->mSeq.mStr.length()) &&
+                            ((trimF + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= get<1>(endBoolR))) {
+                            mTrim.totMismatches = fpMismatches + rpMismatches;
+                            mTrim.trimF = trimF;
+                            mTrim.trimedRefLenth = get<1>(endBoolR) - trimF - locSnpItGrp->rp.mStr.length();
+                            locMap[locSnpItGrp->name] = mTrim;
+                        } else {
+                            continue;
+                        }
+                    }
                 }
             } else {
                 mTrim.totMismatches = fpMismatches + rpMismatches;
@@ -148,6 +189,30 @@ void SnpScanner::groupScanVar(Read* & r1) {
                 mTrim.trimedRefLenth = r1->mSeq.length() - trimF - locSnpItGrp->rp.mStr.length();
                 locMap[locSnpItGrp->name] = mTrim;
             }
+            if(mTrim.totMismatches == 0){
+                break;
+            }
+            
+            // rpData = locSnpItGrp->rp.mStr.c_str();
+            // rpLength = locSnpItGrp->rp.length();
+            // auto endBoolR = doPrimerAlignment(rpData, rpLength, locSnpItGrp->name, readSeq, readLength, r1->mName, true);
+
+            // if(std::min(rpMismatches, get<0>(endBoolR)) > mOptions->mLocSnps.mLocSnpOptions.maxMismatchesPSeq) continue;
+            
+            // if(get<0>(endBoolR) <= rpMismatches){
+            //     if (get<2>(endBoolR) && (get<1>(endBoolR) <= r1->mSeq.mStr.length()) &&
+            //             ((trimF + locSnpItGrp->ft.length() + locSnpItGrp->ref.length() + locSnpItGrp->rt.length() + locSnpItGrp->rp.length()) <= get<1>(endBoolR))) {
+            //         mTrim.totMismatches = fpMismatches + rpMismatches;
+            //         mTrim.trimF = trimF;
+            //         mTrim.trimedRefLenth = get<1>(endBoolR) - trimF - locSnpItGrp->rp.mStr.length();
+            //         locMap[locSnpItGrp->name] = mTrim;
+            //     }
+            // } else {
+            //     mTrim.totMismatches = fpMismatches + rpMismatches;
+            //     mTrim.trimF = trimF;
+            //     mTrim.trimedRefLenth = r1->mSeq.length() - trimF - locSnpItGrp->rp.mStr.length();
+            //     locMap[locSnpItGrp->name] = mTrim;
+            // }
         }
     }
 
